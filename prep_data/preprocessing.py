ec7d8308d92e77280cbf6c6dc7461da412091af4
YuxingZhang <zhangyx233@gmail.com>
2016-11-09 15:21:06 -0500

    Mapping items to numbers

--- prep_data/preprocessing.py
+++ prep_data/preprocessing.py
@@ -1,7 +1,8 @@
 import sys
+import pickle
 
 def process(input_file='../data/yoochoose/yoochoose-clicks.dat', output_file='../data/yoochoose/yoochoose-sessions.dat', 
-            start_time=20140101000000000L, end_time=20141201000000000L, min_freq=2):
+            dict_file='../data/yoochoose/item_dict.pkl', start_time=20140101000000000L, end_time=20141201000000000L, min_freq=2):
     '''
     Process the input data line by line, group data by session number (first attr), and filter by specified
     time stamp & minimum frequency.
@@ -12,6 +13,8 @@ def process(input_file='../data/yoochoose/yoochoose-clicks.dat', output_file='..
     cur_session_items = ""
     cur_session = "-1"
     cur_session_len = 0
+    item_dict = {}
+    num_items = 0
     with open(input_file, "r") as f_in, open(output_file, "w") as f_out:
         for line in f_in:
             session, time, item = line.split(',')[:3]
@@ -19,6 +22,10 @@ def process(input_file='../data/yoochoose/yoochoose-clicks.dat', output_file='..
             if time_n < start_time or time_n > end_time:
                 print "out"
                 continue
+            if item not in item_dict:
+                num_items += 1
+                item_dict[item] = str(num_items)
+            item = item_dict[item]
             if session == cur_session:
                 cur_session_items += " "
                 cur_session_items += item
@@ -38,6 +45,9 @@ def process(input_file='../data/yoochoose/yoochoose-clicks.dat', output_file='..
         if len(buf) > 0:
             f_out.writelines(buf)
 
+    with open(dict_file, 'wb') as dict_out:
+        pickle.dump(item_dict, dict_out)
+
 def parse_time(time_string):
     date, time = time_string.split('T')
     year, month, day = date.split('-')
@@ -46,5 +56,5 @@ def parse_time(time_string):
     return long(year + month + day + hour + minute + second + msecond)
 
 if __name__ == '__main__':
-    process(input_file='../data/yoochoose/yoochoose-clicks.dat') # TODO specify the arguments here or use the default values
+    process(input_file='../data/yoochoose/yoochoose-buys.dat') # TODO specify the arguments here or use the default values
     
